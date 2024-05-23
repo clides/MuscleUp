16f506b557035cd215818835c1b8e32507656941
clides <daniel168.guo@gmail.com>
2024-05-22 22:06:04 -0400

    added rendering of the keypoints

--- MuscleUp.py
+++ MuscleUp.py
@@ -20,20 +20,20 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
         # Recolor Image (mediapipe expects RGB but cv2 gives BGR)
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writable = False # Saves memory
+        image.flags.writeable = False # Saves memory
 
         # Make Detection
         results = pose.process(image)
         
         # Recolor back to BGR
-        image.flags.writable = True
+        image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         # Draw Landmarks
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
         
 
-        cv2.imshow("Image", frame)
+        cv2.imshow("Image", image)
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
